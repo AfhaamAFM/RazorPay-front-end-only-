d43f735d72f25016d8ee640bd0df2bbcbf8b4da7
Afhaam k <afhaamcoc@gmail.com>
2021-12-05 18:31:06 +0530

    first react razor pay commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+
+
+const loadRazorPay=(src)=>{
+  return new Promise(resolve=>{
+  const script = document.createElement('script')
+  script.src=src
+  document.body. appendChild(script)
+
+  script.onload=()=>{
+    resolve(true)
+  }
+  script.onerror=()=>{
+
+    resolve(false)
+  }
+  })
+}
+
+async function showRazorPAy(){
+
+
+const result =await loadRazorPay('https://checkout.razorpay.com/v1/checkout.js')
+if(!result){
+  alert('fail')
+  return
+}
+
+
+const {data} = await axios.get('http://localhost:5000/order/razorpay/payAmount')
+console.log(data)
+
+var options = {
+  "key": "rzp_test_ZanWOoH710Fanr", // Enter the Key ID generated from the Dashboard
+  "amount": data.amount, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+  "currency": data.currency,
+  "name": "Acme Corp",
+  "description": "Test Transaction",
+  "image": "https://example.com/your_logo",
+  "order_id": data.id, //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
+  "handler": function (response){
+      alert(response.razorpay_payment_id);
+      alert(response.razorpay_order_id);
+      alert(response.razorpay_signature)
+  },
+  "prefill": {
+      "name": "Gaurav Kumar",
+      "email": "gaurav.kumar@example.com",
+      "contact": "9999999999"
+  },
+  "notes": {
+      "address": "Razorpay Corporate Office"
+  },
+  "theme": {
+      "color": "#3399cc"
+  }
+};
+var paymentObject = new window.Razorpay(options);
+paymentObject.open();
+}
+
+
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<button onClick={showRazorPAy}>
+  Hi 
+</button>
     </div>
   );
 }
